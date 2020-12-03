c299403515836596201fbd805252ab8a1c1896b6
asd192 <PanshinDA@yandex.ru>
2020-12-03 21:30:04 +0500

    Added new func

--- mainp.py
+++ mainp.py
@@ -11,23 +11,21 @@ class window(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        # Валидация
-        self.ui.lineEdit_in_start_value.setValidator(self.validat_p())
-        self.ui.lineEdit_in_end_value.setValidator(self.validat_p())
-        self.ui.lineEdit_out_start_value.setValidator(self.validat_p())
-        self.ui.lineEdit_out_end_value.setValidator(self.validat_p())
+        # Валидация полей Вход, Выход, Шкала ПВИ параметров прибора
+        self.ui.lineEdit_in_start_value.setValidator(self.validat_param())
+        self.ui.lineEdit_in_end_value.setValidator(self.validat_param())
+        self.ui.lineEdit_out_start_value.setValidator(self.validat_param())
+        self.ui.lineEdit_out_end_value.setValidator(self.validat_param())
 
-        self.ui.lineEdit_pvi_scale_start.setValidator(self.validat_p())
-        self.ui.lineEdit_pvi_scale_start.setValidator(self.validat_p())
+        self.ui.lineEdit_pvi_scale_start.setValidator(self.validat_param())
+        self.ui.lineEdit_pvi_scale_start.setValidator(self.validat_param())
 
+        # Валиддация показаний Выход ИРТ, Выход ПВИ
         self.ui.lineEdit_out_irt_value_5.setValidator(self.validat_in_out())
         self.ui.lineEdit_out_irt_value_25.setValidator(self.validat_in_out())
         self.ui.lineEdit_out_irt_value_50.setValidator(self.validat_in_out())
         self.ui.lineEdit_out_irt_value_75.setValidator(self.validat_in_out())
         self.ui.lineEdit_out_irt_value_95.setValidator(self.validat_in_out())
-
-        self.ui.lineEdit_out_24_value_0.setValidator(self.valdat_24())
-        self.ui.lineEdit_out_24_value_820.setValidator(self.valdat_24())
 
         self.ui.lineEdit_out_pvi_value_5.setValidator(self.validat_in_out())
         self.ui.lineEdit_out_pvi_value_25.setValidator(self.validat_in_out())
@@ -35,20 +33,52 @@ class window(QtWidgets.QMainWindow):
         self.ui.lineEdit_out_pvi_value_75.setValidator(self.validat_in_out())
         self.ui.lineEdit_out_pvi_value_95.setValidator(self.validat_in_out())
 
+        # Валидация полей Выход 24В
+        self.ui.lineEdit_out_24_value_0.setValidator(self.valdat_24())
+        self.ui.lineEdit_out_24_value_820.setValidator(self.valdat_24())
+
         # Чекбокс ПВИ
         self.ui.checkBox_pvi.stateChanged.connect(lambda check=self.ui.checkBox_pvi.isChecked(): self.select_pvi(check))
 
-        # Установка входных значений выхода
+        # Установка входных значений выходов
         self.ui.lineEdit_in_start_value.textChanged.connect(self.out_irt_in)
         self.ui.lineEdit_in_end_value.textChanged.connect(self.out_irt_in)
 
-    def validat_in_out(self):
-        return QtGui.QRegExpValidator(QtCore.QRegExp("^[+-]?\d{,5}(?:[\.,]\d{,3})?$"))
+        self.ui.lineEdit_pvi_scale_start.textChanged.connect(self.out_pvi_in)
+        self.ui.lineEdit_pvi_scale_end.textChanged.connect(self.out_pvi_in)
 
-    def validat_para(self):
+        # Установка параметров входа
+        self.ui.comboBox_in_signal_type.activated.connect(self.parametr_in_signal)
+
+    def parametr_in_signal(self):
+        """ Устанавливает параметры прибора(входные значения) """
+        sensor_type = {
+            "ТП-ХК": (-50, 600),
+            "ТП-ХА": (-50, 1300),
+            "ТСМ-50М": (-50, 200),
+            "ТСМ-100М": (-50, 200),
+            "ТСМ-50П": (-50, 600),
+            "ТСМ-100П": (-50, 600),
+        }
+
+        sensor_type_key = self.ui.comboBox_in_signal_type.currentText()
+        if sensor_type_key == "мА":
+            self.ui.lineEdit_in_start_value.setText("")
+            self.ui.lineEdit_in_end_value.setText("")
+        else:
+            self.ui.lineEdit_in_start_value.setText(str(sensor_type.get(sensor_type_key)[0]))
+            self.ui.lineEdit_in_end_value.setText(str(sensor_type.get(sensor_type_key)[1]))
+
+    def validat_param(self):
+        """ Валидация полей Вход, Выход, Шкала ПВИ параметров прибора """
         return QtGui.QRegExpValidator(QtCore.QRegExp("^[+-]?\d{,6}(?:[\.,]\d{,3})?$"))
 
+    def validat_in_out(self):
+        """ Валиддация показаний Выход ИРТ, Выход ПВИ """
+        return QtGui.QRegExpValidator(QtCore.QRegExp("^[+-]?\d{,5}(?:[\.,]\d{,3})?$"))
+
     def valdat_24(self):
+        """ Валидация полей Выход 24В """
         return QtGui.QRegExpValidator(QtCore.QRegExp("^[+-]?\d{,2}(?:[\.,]\d{,3})?$"))
 
     def select_pvi(self, pvi_on):
@@ -70,21 +100,47 @@ class window(QtWidgets.QMainWindow):
 
     def out_irt_in(self):
         """ Расчет требуемых входных """
+        values = ['']
         try:
             in_start = self.ui.lineEdit_in_start_value.text().replace(',', '.')
             in_end = self.ui.lineEdit_in_end_value.text().replace(',', '.')
 
-            values = []
             for i in (0.05, 0.25, 0.5, 0.75, 0.95):
                 values.append(str((abs(float(in_start)) + abs(float(in_end))) * i))
 
-            self.ui.lineEdit_out_irt_in_5.setText(values[0])
-            self.ui.lineEdit_out_irt_in_25.setText(values[1])
-            self.ui.lineEdit_out_irt_in_50.setText(values[2])
-            self.ui.lineEdit_out_irt_in_75.setText(values[3])
-            self.ui.lineEdit_out_irt_in_95.setText(values[4])
+            self.ui.lineEdit_out_irt_in_5.setText(values[1])
+            self.ui.lineEdit_out_irt_in_25.setText(values[2])
+            self.ui.lineEdit_out_irt_in_50.setText(values[3])
+            self.ui.lineEdit_out_irt_in_75.setText(values[4])
+            self.ui.lineEdit_out_irt_in_95.setText(values[5])
         except:
-            pass
+            self.ui.lineEdit_out_irt_in_5.setText(values[0])
+            self.ui.lineEdit_out_irt_in_25.setText(values[0])
+            self.ui.lineEdit_out_irt_in_50.setText(values[0])
+            self.ui.lineEdit_out_irt_in_75.setText(values[0])
+            self.ui.lineEdit_out_irt_in_95.setText(values[0])
+
+    def out_pvi_in(self):
+        """ Расчет требуемых входных ПВИ"""
+        values = ['']
+        try:
+            in_start = self.ui.lineEdit_pvi_scale_start.text().replace(',', '.')
+            in_end = self.ui.lineEdit_pvi_scale_end.text().replace(',', '.')
+
+            for i in (0.05, 0.25, 0.5, 0.75, 0.95):
+                values.append(str((abs(float(in_start)) + abs(float(in_end))) * i))
+
+            self.ui.lineEdit_out_pvi_in_5.setText(values[1])
+            self.ui.lineEdit_out_pvi_in_25.setText(values[2])
+            self.ui.lineEdit_out_pvi_in_50.setText(values[3])
+            self.ui.lineEdit_out_pvi_in_75.setText(values[4])
+            self.ui.lineEdit_out_pvi_in_95.setText(values[5])
+        except:
+            self.ui.lineEdit_out_pvi_in_5.setText(values[0])
+            self.ui.lineEdit_out_pvi_in_25.setText(values[0])
+            self.ui.lineEdit_out_pvi_in_50.setText(values[0])
+            self.ui.lineEdit_out_pvi_in_75.setText(values[0])
+            self.ui.lineEdit_out_pvi_in_95.setText(values[0])
 
 
 app = QtWidgets.QApplication([])
